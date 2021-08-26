e0e6e0c4f78b83a9476a846c694bb66da4fc8f23
joemathai <jmathai@isi.edu>
2021-08-26 13:03:05 -0700

    initial commit

---
+++ adversarial_pretrain/src/models/timm_model_loader.py
@@ -0,0 +1,135 @@
+from functools import partial
+
+import timm
+import torch
+import torch.nn as nn
+from torchinfo import summary
+
+
+def to_status(m, status):
+    if hasattr(m, 'batch_type'):
+        m.batch_type = status
+
+
+to_clean_status = partial(to_status, status='clean')
+to_adv_status = partial(to_status, status='adv')
+to_mix_status = partial(to_status, status='mix')
+
+
+class MixLayerNorm(nn.Module):
+    '''
+    if the dimensions of the tensors from dataloader is [N, 3, 224, 224]
+    that of the inputs of the MixBatchNorm2d should be [2*N, 3, 224, 224].
+    If you set batch_type as 'mix', this network will using one batchnorm (main bn) to calculate the features corresponding to[:N, 3, 224, 224],
+    while using another batch normalization (auxiliary bn) for the features of [N:, 3, 224, 224].
+    During training, the batch_type should be set as 'mix'.
+    During validation, we only need the results of the features using some specific batchnormalization.
+    if you set batch_type as 'clean', the features are calculated using main bn; if you set it as 'adv', the features are calculated using auxiliary bn.
+    Usually, we use to_clean_status, to_adv_status, and to_mix_status to set the batch_type recursively. It should be noticed that the batch_type should be set as 'adv' while attacking.
+    '''
+
+    def __init__(self, normalized_shape, eps, elementwise_affine, weight, bias):
+        super().__init__()
+        self.ln = nn.LayerNorm(normalized_shape, eps=eps, elementwise_affine=elementwise_affine)
+        self.ln_aux = nn.LayerNorm(normalized_shape, eps=eps, elementwise_affine=elementwise_affine)
+        self.batch_type = 'clean'
+        with torch.no_grad():
+            self.ln.weight.copy_(weight)
+            self.ln.bias.copy_(bias)
+            self.ln_aux.weight.copy_(weight)
+            self.ln_aux.bias.copy_(bias)
+
+    def forward(self, input):
+        if self.batch_type == 'adv':
+            input = self.ln_aux(input)
+        elif self.batch_type == 'clean':
+            input = self.ln(input)
+        else:
+            assert self.batch_type == 'mix'
+            batch_size = input.shape[0]
+            input0 = self.ln(input[:batch_size // 2])
+            input1 = self.ln_aux(input[batch_size // 2:])
+            input = torch.cat((input0, input1), 0)
+        return input
+
+
+class MixBatchNorm2d(nn.Module):
+    '''
+    if the dimensions of the tensors from dataloader is [N, 3, 224, 224]
+    that of the inputs of the MixBatchNorm2d should be [2*N, 3, 224, 224].
+    If you set batch_type as 'mix', this network will using one batchnorm (main bn) to calculate the features corresponding to[:N, 3, 224, 224],
+    while using another batch normalization (auxiliary bn) for the features of [N:, 3, 224, 224].
+    During training, the batch_type should be set as 'mix'.
+    During validation, we only need the results of the features using some specific batchnormalization.
+    if you set batch_type as 'clean', the features are calculated using main bn; if you set it as 'adv', the features are calculated using auxiliary bn.
+    Usually, we use to_clean_status, to_adv_status, and to_mix_status to set the batch_type recursively. It should be noticed that the batch_type should be set as 'adv' while attacking.
+    '''
+
+    def __init__(self, num_features, eps=1e-5, momentum=0.1, affine=True, track_running_stats=True,
+                 weight=None, bias=None, running_mean=None, running_var=None, num_batches_tracked=0):
+        super().__init__()
+        self.bn = nn.BatchNorm2d(num_features, eps=eps, momentum=momentum, affine=affine,
+                                 track_running_stats=track_running_stats)
+        self.aux_bn = nn.BatchNorm2d(num_features, eps=eps, momentum=momentum, affine=affine,
+                                     track_running_stats=track_running_stats)
+        self.batch_type = 'clean'
+        # note: not copying the num_batches tracked
+        with torch.no_grad():
+            if all(args is not None for args in (weight, bias, running_mean, running_var)):
+                self.bn.weight.copy_(weight)
+                self.bn.bias.copy_(bias)
+                self.bn.running_mean.copy_(running_mean)
+                self.bn.running_var.copy_(running_var)
+                self.aux_bn.weight.copy_(weight)
+                self.aux_bn.bias.copy_(bias)
+                self.aux_bn.running_mean.copy_(running_mean)
+                self.aux_bn.running_var.copy_(running_var)
+
+    def forward(self, input):
+        if self.batch_type == 'adv':
+            input = self.aux_bn(input)
+        elif self.batch_type == 'clean':
+            input = self.bn(input)
+        else:
+            assert self.batch_type == 'mix'
+            batch_size = input.shape[0]
+            input0 = self.bn(input[:batch_size // 2])
+            input1 = self.aux_bn(input[batch_size // 2:])
+            input = torch.cat((input0, input1), 0)
+        return input
+
+
+class MixBNModelBuilder(torch.nn.Module):
+    def __init__(self, model_type, pretrained=True, mix_bn=False, mean=(0.5, 0.5, 0.5), std=(0.5, 0.5, 0.5)):
+        super().__init__()
+        self.model = timm.create_model(model_type, pretrained=pretrained, num_classes=2)
+        summary(self.model)
+        if mix_bn:
+            print("replacing the BatchNorm2d with MixBatchNorm2d")
+            self.replace_bn_layers(self.model)
+        self.register_buffer('mean', torch.tensor(mean))
+        self.register_buffer('std', torch.tensor(std))
+
+    def forward(self, imgs):
+        imgs = imgs.permute(0, 2, 3, 1)
+        imgs = (imgs - self.mean) / (self.std + 1e-8)
+        return self.model(imgs.permute(0, 3, 1, 2))
+
+    @staticmethod
+    def replace_bn_layers(model):
+        # if the model is adv then use auxillary batchnorm
+        for n, module in model.named_children():
+            if len(list(module.children())) > 0:
+                MixBNModelBuilder.replace_bn_layers(module)
+            if isinstance(module, nn.BatchNorm2d):
+                setattr(model, n, MixBatchNorm2d(num_features=module.num_features,
+                                                 eps=module.eps, momentum=module.momentum,
+                                                 affine=module.affine,
+                                                 track_running_stats=module.track_running_stats,
+                                                 running_mean=module.running_mean,
+                                                 running_var=module.running_var))
+            elif isinstance(module, nn.LayerNorm):
+                setattr(model, n, MixLayerNorm(module.normalized_shape, module.eps,
+                                               module.elementwise_affine, module.weight, module.bias))
+            else:
+                continue
---
+++ adversarial_pretrain/src/train.py
@@ -0,0 +1,186 @@
+import argparse
+import os
+import pathlib
+import json
+import shutil
+
+import numpy as np
+import torch
+import wandb
+import torchvision
+
+from adversarial_pretrain.src.models.timm_model_loader import MixBNModelBuilder
+
+torch.backends.cudnn.benchmark = True
+DEVICE = torch.device('cuda')
+
+
+class AdvPreTrain:
+    def __init__(self, config):
+        seed = np.random.randint(99999)
+        assert pathlib.Path(config).is_file(), f"provided config({config}) doesn't exist"
+        with open(config, 'r') as fh:
+            self.config = json.load(fh)
+
+        self.project_name = self.config['project_name']
+        self.model_type = self.config['model_type']
+        self.train_type = self.config['train_type']
+        self.pretrain_dataset = self.config['pretrain_dataset']
+        self.pretrain_classes = self.config['pretrain_classes']
+        self.finetune_dataset = self.config['finetune_dataset']
+        self.finetune_classes = self.config['finetune_classes']
+        self.save_path = self.config['save_path']
+        self.exp_name = f"{self.train_type}_{self.model_type}_seed_{seed}_pretrain_{self.pretrain_dataset}"\
+                        f"_finetune_{self.finetune_dataset}"
+
+        # define model and pretraining dataset
+        self.model = MixBNModelBuilder(model_type=self.model_type, pretrained=False, mix_bn=False)
+        base_dir = pathlib.Path(f"{self.save_path}/{self.project_name}/{self.exp_name}")
+        base_dir.mkdir(parents=True, exist_ok=True)
+
+        self.pretrain_cur_model = base_dir / "pretrain_cur.pth"
+        self.pretrain_best_model = base_dir / "pretrain_best.pth"
+        self.finetune_cur_model = base_dir / "finetune_cur.pth"
+        self.finetune_best_model = base_dir / "finetune_best.pth"
+
+        # initialize wandb
+        self.wandb_run = wandb.init(project=self.project_name, dir=os.getenv('TMPDIR'),
+                                    name=self.exp_name, id=self.exp_name, reinit=True)
+
+    @staticmethod
+    def accuracy(output, target, topk=(1,)):
+        """Computes the accuracy over the k top predictions for the specified values of k"""
+        with torch.no_grad():
+            maxk = max(topk)
+            batch_size = target.size(0)
+            _, pred = output.topk(maxk, 1, True, True)
+            pred = pred.t()
+            correct = pred.eq(target.view(1, -1).expand_as(pred))
+            res = []
+            for k in topk:
+                correct_k = correct[:k].reshape(-1).float().sum(0, keepdim=True)
+                res.append(correct_k.mul_(100.0 / batch_size))
+            return res
+
+    @staticmethod
+    def snapshot_gpu(model, optimizer, epoch, path):
+        torch.save({'model': model.state_dict(),
+                    'optimizer': optimizer.state_dict(),
+                    'cur_epoch': epoch}, path)
+
+    @staticmethod
+    def load_snapshot(model, path, device):
+        snapshot = torch.load(path, map_location=device)
+        model.load_state_dict(snapshot['model'])
+        model.to(device)
+
+    def pretrain_imagenet(self):
+        pretrain_epochs = 100
+        batch_size = 128
+        pretrain_lr = 1e-2
+        pretrain_wd = 1e-3
+
+        # copy imagenet data locally
+        imagenet_root = pathlib.Path(f"{os.getenv('TMPDIR')}/imagenet_data")
+        imagenet_root.mkdir(exist_ok=True, parents=True)
+        shutil.copy("/nas/vista-ssd01/batl/public_datasets/ImageNet/ILSVRC2012_img_train.tar",
+                    str(imagenet_root))
+        shutil.copy("/nas/vista-ssd01/batl/public_datasets/ImageNet/ILSVRC2012_img_val.tar",
+                    str(imagenet_root))
+        shutil.copy("/nas/vista-ssd01/batl/public_datasets/ImageNet/ILSVRC2012_devkit_t12.tar.gz",
+                    str(imagenet_root))
+
+        pretrain_train_dataset = torchvision.datasets.ImageNet(root="",
+                                                               split="train",
+                                                               transform=torchvision.transforms.Compose([
+                                                                   torchvision.transforms.RandomResizedCrop(224),
+                                                                   torchvision.transforms.RandomHorizontalFlip(),
+                                                                   torchvision.transforms.ToTensor()
+                                                               ]))
+        train_dataloader = torch.utils.data.DataLoader(pretrain_train_dataset, batch_size=batch_size,
+                                                       shuffle=True, num_workers=4, pin_memory=True, drop_last=True,
+                                                       worker_init_fn=lambda wid: np.random.seed(
+                                                           np.random.get_state()[1][0] + wid)
+                                                       )
+        pretrain_valid_dataset = torchvision.datasets.ImageNet(root="",
+                                                               split="val",
+                                                               transform=torchvision.transforms.Compose([
+                                                                   torchvision.transforms.Resize(256),
+                                                                   torchvision.transforms.CenterCrop(224),
+                                                                   torchvision.transforms.ToTensor()
+                                                               ]))
+        valid_dataloader = torch.utils.data.DataLoader(pretrain_valid_dataset, batch_size=batch_size,
+                                                       shuffle=False, num_workers=0, pin_memory=False, drop_last=False)
+
+        criterion = torch.nn.CrossEntropyLoss().to(DEVICE)
+        optimizer = torch.optim.AdamW(self.model.parameters(), lr=pretrain_lr, weight_decay=pretrain_wd)
+        scheduler = torch.optim.lr_scheduler.StepLR(optimizer, step_size=20, gamma=0.5)
+        scaler = torch.cuda.amp.GradScaler()
+
+        best_val_acc1 = -np.inf
+
+        for epoch in range(pretrain_epochs):
+            losses = list()
+            acc1s = list()
+            acc5s = list()
+            for batch_idx, (data, labels) in enumerate(train_dataloader):
+                data, labels = data.float().to(DEVICE, non_blocking=True), labels.long().to(DEVICE, non_blocking=True)
+
+                optimizer.zero_grad()
+                with torch.cuda.amp.autocast():
+                    preds = self.model(data)
+                    loss = criterion(preds, labels)
+
+                acc1, acc5 = AdvPreTrain.accuracy(preds, labels, topk=(1, 5))
+                print(f"epoch:{epoch + 1}, batch:{batch_idx + 1}, loss:{loss.item():.6f}, acc1:{acc1}, acc5:{acc5}")
+                wandb.log({'pretrain/loss': loss.item()})
+                losses.append(loss.item())
+                acc1s.append(acc1)
+                acc5s.append(acc5)
+
+                scaler.scale(loss).backward()
+                scaler.step(optimizer)
+
+            wandb.log({'pretrain/epoch_avg_loss': np.mean(losses),
+                       'pretrain/epoch_avg_acc1': np.mean(acc1s),
+                       'pretrain/epoch_avg_acc5': np.mean(acc5s)})
+            scheduler.step()
+
+            # validate
+            with torch.no_grad():
+                # note this averaging is not exactly correct because of change in batch size amongst validation
+                # but this should be good enough for validation purposes
+                val_losses = list()
+                val_acc1s = list()
+                val_acc5s = list()
+                for idx, (data, labels) in enumerate(valid_dataloader):
+                    data, labels = data.float().to(DEVICE), labels.long().to(DEVICE)
+                    preds = self.model(data)
+                    acc1, acc5 = AdvPreTrain.accuracy(preds, labels, topk=(1, 5))
+                    loss = criterion(preds, labels)
+                    val_losses.append(loss.item())
+                    val_acc1s.append(acc1)
+                    val_acc5s.append(acc5)
+                wandb.log({'pretrain/validation_loss:': np.mean(val_losses),
+                           'pretrain/validation_acc1:': np.mean(val_acc1s),
+                           'pretrain/validation_acc5': np.mean(val_acc5s)})
+
+                if np.mean(val_acc1s) > best_val_acc1:
+                    best_val_acc1 = np.mean(val_acc1s)
+                    AdvPreTrain.snapshot_gpu(self.model, optimizer, epoch, self.pretrain_best_model)
+
+    def finetune(self):
+        pass
+
+
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser(description='Parser for BATL adv. training')
+    parser.add_argument('--config', type=str, required=True)
+    args = parser.parse_args()
+
+    if not torch.cuda.is_available():
+        print('cuda is not available exiting....')
+        exit(1)
+
+    trainer = AdvPreTrain(args.config)
+    trainer.pretrain_imagenet()
